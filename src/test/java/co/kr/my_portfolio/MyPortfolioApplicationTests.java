7d29487835e334b499dfa6a4bb9dc961195a00b5
이건우 <qkskck3@gmail.com>
2025-05-25 12:55:15 +0900

    Initial commit

---
+++ src/test/java/co/kr/my_portfolio/MyPortfolioApplicationTests.java
@@ -0,0 +1,13 @@
+package co.kr.my_portfolio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyPortfolioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
