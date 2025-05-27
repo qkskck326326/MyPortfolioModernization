3a6e3d6d53d67defa7d18f5d7014513288268bfc
이건우 <qkskck3@gmail.com>
2025-05-27 16:41:03 +0900

    사용자 엔티티 및 권한(Enum) 정의

---
+++ src/main/java/co/kr/my_portfolio/user/domain/User.java
@@ -0,0 +1,36 @@
+package co.kr.my_portfolio.user.domain;
+
+import co.kr.my_portfolio.common.domain.BaseTimeEntity;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class User extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true, length = 50)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false, length = 30)
+    private String nickname;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Role role;
+
+    @Builder
+    public User(String email, String password, String nickname, Role role) {
+        this.email = email;
+        this.password = password;
+        this.nickname = nickname;
+        this.role = role;
+    }
+}
