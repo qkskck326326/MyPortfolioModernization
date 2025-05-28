846ac99ef7b7933feab807209caa22f849338faa
이건우 <qkskck3@gmail.com>
2025-05-28 16:16:03 +0900

    Logout 및 Reissue API 추가

--- src/main/java/co/kr/my_portfolio/auth/controller/AuthController.java
+++ src/main/java/co/kr/my_portfolio/auth/controller/AuthController.java
@@ -1,7 +1,9 @@
 package co.kr.my_portfolio.auth.controller;
 
-import co.kr.my_portfolio.auth.dto.EmailLoginRequest;
-import co.kr.my_portfolio.auth.dto.TokenResponse;
+import co.kr.my_portfolio.auth.dto.login.EmailLoginRequest;
+import co.kr.my_portfolio.auth.dto.jwt.ReissueRequest;
+import co.kr.my_portfolio.auth.dto.jwt.TokenResponse;
+import co.kr.my_portfolio.auth.dto.login.LogoutRequest;
 import co.kr.my_portfolio.auth.service.LoginService;
 import co.kr.my_portfolio.common.dto.ApiResponse;
 import jakarta.validation.Valid;
@@ -13,21 +15,34 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/login")
+@RequestMapping("/api")
 @RequiredArgsConstructor
 public class AuthController {
 
     private final LoginService loginService;
 
-    /**
-     * 카카오 로그인 API
-     */
-
-    @PostMapping("/email")
+    // 이메일 로그인
+    @PostMapping("/login/email")
     public ResponseEntity<ApiResponse<TokenResponse>> loginWithEmail(
             @Valid @RequestBody EmailLoginRequest request) {
 
         TokenResponse tokens = loginService.login(request);
         return ResponseEntity.ok(ApiResponse.success(tokens));
     }
+    
+    // 로그아웃
+    @PostMapping("/logout")
+    public ResponseEntity<ApiResponse<Void>> logout(@RequestBody @Valid LogoutRequest request) {
+        loginService.logout(request.getRefreshToken());
+        return ResponseEntity.ok(ApiResponse.success(null, "로그아웃 되었습니다."));
+    }
+    
+    // 토큰 재생성
+    @PostMapping("/reissue")
+    public ResponseEntity<ApiResponse<TokenResponse>> reissue(@RequestBody @Valid ReissueRequest request) {
+        TokenResponse tokenResponse = loginService.reissue(request.getRefreshToken());
+        return ResponseEntity.ok(ApiResponse.success(tokenResponse));
+    }
+
+
 }
