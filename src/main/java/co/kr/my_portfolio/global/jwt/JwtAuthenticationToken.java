d0ab70783b1c5a9247c5fb62a13a9e1c8df2f58d
이건우 <qkskck3@gmail.com>
2025-05-29 15:49:24 +0900

    커스텀 인증 객체 구현

---
+++ src/main/java/co/kr/my_portfolio/global/jwt/JwtAuthenticationToken.java
@@ -0,0 +1,33 @@
+package co.kr.my_portfolio.global.jwt;
+
+import lombok.Getter;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.GrantedAuthority;
+
+import java.util.Collection;
+import java.util.Date;
+
+@Getter
+public class JwtAuthenticationToken extends UsernamePasswordAuthenticationToken {
+
+    private final String userId;
+    private final String role;
+    private final Date expiration;
+
+    public JwtAuthenticationToken(String userId, String role, Date expiration, Collection<? extends GrantedAuthority> authorities) {
+        super(userId, null, authorities); // principal = userId, credentials = null
+        this.userId = userId;
+        this.role = role;
+        this.expiration = expiration;
+    }
+
+    @Override
+    public Object getPrincipal() {
+        return userId;
+    }
+
+    @Override
+    public Object getCredentials() {
+        return null;
+    }
+}
