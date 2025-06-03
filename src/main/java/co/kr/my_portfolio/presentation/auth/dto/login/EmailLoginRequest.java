cd45c7fec0e61341830ef6f77ab298ad4183b1d3
이건우 <qkskck3@gmail.com>
2025-06-03 20:08:54 +0900

    Request DTO → Command 변환 구조 적용

--- src/main/java/co/kr/my_portfolio/presentation/auth/dto/login/EmailLoginRequest.java
+++ src/main/java/co/kr/my_portfolio/presentation/auth/dto/login/EmailLoginRequest.java
@@ -1,16 +1,20 @@
 package co.kr.my_portfolio.presentation.auth.dto.login;
 
+import co.kr.my_portfolio.application.auth.dto.EmailLoginCommand;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 
 @Getter
-public class EmailLoginRequest implements LoginRequest {
-
+public class EmailLoginRequest{
     @Email(message = "유효한 이메일 형식을 입력해주세요.")
     @NotBlank(message = "이메일은 필수입니다.")
     private String email;
 
     @NotBlank(message = "비밀번호는 필수입니다.")
     private String password;
+
+    public EmailLoginCommand toCommand() {
+        return new EmailLoginCommand(email, password);
+    }
 }
