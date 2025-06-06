85d39414f56ac2eef84c12232c67d50a4963b0fd
이건우 <qkskck3@gmail.com>
2025-06-07 00:15:31 +0900

    비관적 락 걸고 조회하기 쿼리 추가

--- src/main/java/co/kr/my_portfolio/domain/portfolio/repository/PortfolioRepository.java
+++ src/main/java/co/kr/my_portfolio/domain/portfolio/repository/PortfolioRepository.java
@@ -1,9 +1,13 @@
 package co.kr.my_portfolio.domain.portfolio.repository;
 
 import co.kr.my_portfolio.domain.portfolio.Portfolio;
+import jakarta.persistence.LockModeType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
 
 public interface PortfolioRepository extends JpaRepository<Portfolio, Long> {
     @Query("""
@@ -13,4 +17,8 @@ public interface PortfolioRepository extends JpaRepository<Portfolio, Long> {
         WHERE p.id = :id
     """)
     Portfolio findWithTagsById(@Param("id") Long id);
+
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    @Query("select p from Portfolio p where p.id = :id")
+    Optional<Portfolio> findByIdForUpdate(@Param("id") Long id);
 }
