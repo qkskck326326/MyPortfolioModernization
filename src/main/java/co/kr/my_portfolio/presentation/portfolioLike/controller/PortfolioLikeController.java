e33e42ce9b4cccd87fde6b02b9670f2bb27e4e74
이건우 <qkskck3@gmail.com>
2025-06-07 00:19:14 +0900

    PortfolioLikeController 생성 및 토큰 엔드포인트 추가

---
+++ src/main/java/co/kr/my_portfolio/presentation/portfolioLike/controller/PortfolioLikeController.java
@@ -0,0 +1,24 @@
+package co.kr.my_portfolio.presentation.portfolioLike.controller;
+
+import co.kr.my_portfolio.application.portfolioLike.PortfolioLikeService;
+import co.kr.my_portfolio.global.response.ApiResponse;
+import co.kr.my_portfolio.presentation.portfolioLike.dto.PortfolioLikeToggleRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/portfolioLike")
+@RequiredArgsConstructor
+public class PortfolioLikeController {
+    private final PortfolioLikeService portfolioLikeService;
+
+    // 포트폴리오 좋아요 표시 토글
+    @PostMapping
+    public ResponseEntity<ApiResponse<String>> portfolioLikeToggle(
+            @RequestBody PortfolioLikeToggleRequest request
+    ){
+        portfolioLikeService.portfolioLikeToggle(request.getPortfolioId());
+        return null;
+    }
+}
