1fcfb25b84105f9137f1ba7b54a7d648007b712b
이건우 <qkskck3@gmail.com>
2025-06-07 18:26:43 +0900

    PortfolioCard record -> class 수정

--- src/main/java/co/kr/my_portfolio/domain/portfolio/PortfolioCard.java
+++ src/main/java/co/kr/my_portfolio/domain/portfolio/PortfolioCard.java
@@ -1,9 +1,28 @@
 package co.kr.my_portfolio.domain.portfolio;
 
 import com.querydsl.core.annotations.QueryProjection;
+import lombok.Builder;
+import lombok.Getter;
 
-public record PortfolioCard(Long id, String thumbnail, String title, int likeCount, String nickname) {
+import java.time.LocalDateTime;
+
+@Getter
+public class PortfolioCard {
+    private final Long id;
+    private final String thumbnail;
+    private final String title;
+    private final int likeCount;
+    private final String nickname;
+    private final LocalDateTime createdAt;
+
+    @Builder
     @QueryProjection
-    public PortfolioCard {
+    public PortfolioCard(Long id, String thumbnail, String title, int likeCount, String nickname, LocalDateTime createdAt) {
+        this.id = id;
+        this.thumbnail = thumbnail;
+        this.title = title;
+        this.likeCount = likeCount;
+        this.nickname = nickname;
+        this.createdAt = createdAt;
     }
 }
