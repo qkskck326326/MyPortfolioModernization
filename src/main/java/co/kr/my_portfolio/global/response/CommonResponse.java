526276ced05ead57166b92df51e92bff40e47195
이건우 <qkskck3@gmail.com>
2025-06-08 23:33:13 +0900

    swagger 문서화 및 설명, 예시 추가

--- src/main/java/co/kr/my_portfolio/global/response/CommonResponse.java
+++ src/main/java/co/kr/my_portfolio/global/response/CommonResponse.java
@@ -1,14 +1,20 @@
 package co.kr.my_portfolio.global.response;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
+@Schema(name = "CommonResponse - API 공통 응답 모델", description = """
+        API 공통 응답입니다.
+        """)
 @Getter
 @AllArgsConstructor(staticName = "of")
 public class CommonResponse<T> {
-
+    @Schema(description = "해당 요청의 성공 or 실패 여부", example = "true")
     private final boolean success;
+    @Schema(description = "해당 요청 결과의 데이터", example = "true")
     private final T data;
+    @Schema(description = "해당 요청 결과의 메세지", example = "true")
     private final String message;
 
     // 성공 시 (데이터만)
