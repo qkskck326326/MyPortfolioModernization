928e35e21a32f9fb8800af1056ac0fa238776e2e
이건우 <qkskck3@gmail.com>
2025-06-09 00:38:03 +0900

    /public 으로 끝나는 api 허용하도록 수정

--- src/main/java/co/kr/my_portfolio/infrastructure/config/SecurityConfig.java
+++ src/main/java/co/kr/my_portfolio/infrastructure/config/SecurityConfig.java
@@ -37,8 +37,8 @@ public class SecurityConfig {
                 .sessionManagement(session -> session.sessionCreationPolicy(
                         org.springframework.security.config.http.SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(request -> request.getRequestURI().endsWith("/public")).permitAll()
                         .requestMatchers("/api/users/signup", "/api/login/**").permitAll()
-                        .requestMatchers(HttpMethod.GET, "/api/public/**").permitAll()
                         .requestMatchers("/swagger-ui/**", "/v3/api-docs/**", "/swagger-ui.html").permitAll()
                         .anyRequest().authenticated()
                 )
