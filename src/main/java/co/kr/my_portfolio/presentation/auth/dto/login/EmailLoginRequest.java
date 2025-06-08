04fedb405bb500be70d83bc030a5d559889a4632
이건우 <qkskck3@gmail.com>
2025-06-08 23:32:59 +0900

    swagger 예시 추가

--- src/main/java/co/kr/my_portfolio/presentation/auth/dto/login/EmailLoginRequest.java
+++ src/main/java/co/kr/my_portfolio/presentation/auth/dto/login/EmailLoginRequest.java
@@ -1,10 +1,14 @@
 package co.kr.my_portfolio.presentation.auth.dto.login;
 
 import co.kr.my_portfolio.application.auth.dto.EmailLoginCommand;
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 
+@Schema(name = "EmailLoginRequest - 이메일 로그인 요청", description = """
+        이메일 + 패스워드로 로그인하는 전통적인 로그인 방식 Request 입니다.
+        """)
 @Getter
 public class EmailLoginRequest{
     @Email(message = "유효한 이메일 형식을 입력해주세요.")
