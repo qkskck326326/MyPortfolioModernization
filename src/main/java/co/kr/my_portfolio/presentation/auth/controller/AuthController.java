c94e57c3fc20d4c0679f7fa6bdb1f88119f700a0
이건우 <qkskck3@gmail.com>
2025-06-08 22:59:31 +0900

    AuthController Swagger Controller 문서화

--- src/main/java/co/kr/my_portfolio/presentation/auth/controller/AuthController.java
+++ src/main/java/co/kr/my_portfolio/presentation/auth/controller/AuthController.java
@@ -1,10 +1,13 @@
 package co.kr.my_portfolio.presentation.auth.controller;
 
-import co.kr.my_portfolio.application.auth.dto.EmailLoginCommand;
 import co.kr.my_portfolio.presentation.auth.dto.jwt.TokenResponse;
 import co.kr.my_portfolio.application.auth.LoginService;
 import co.kr.my_portfolio.global.response.ApiResponse;
 import co.kr.my_portfolio.presentation.auth.dto.login.EmailLoginRequest;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -14,6 +17,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "Auth", description = "인증/인가 관련 API")
 @RestController
 @RequestMapping("/api")
 @RequiredArgsConstructor
@@ -22,6 +26,11 @@ public class AuthController {
     private final LoginService loginService;
 
     // 이메일 로그인
+    @Operation(summary = "이메일 로그인",
+            description = """
+                    API 사용 조건
+                    - email, password 생략 불가.
+                    """)
     @PostMapping("/login/email")
     public ResponseEntity<ApiResponse<TokenResponse>> loginWithEmail(
             @Valid @RequestBody EmailLoginRequest request) {
@@ -31,6 +40,12 @@ public class AuthController {
     }
 
     // 로그아웃
+    @SecurityRequirement(name = "JWT")
+    @Operation(summary = "로그아웃",
+            description = """
+                    API 사용 조건
+                    - JWT 인증( 로그인 )
+                    """)
     @PostMapping("/logout")
     public ResponseEntity<ApiResponse<Void>> logout(HttpServletRequest request) {
         String refreshToken = extractTokenFromHeader(request);
@@ -39,6 +54,12 @@ public class AuthController {
     }
 
     // 토큰 재생성
+    @SecurityRequirement(name = "JWT")
+    @Operation(summary = "Access 토큰 재생성",
+            description = """
+                    API 사용 조건
+                    - JWT 인증( Refresh Token )
+                    """)
     @PostMapping("/reissue")
     public ResponseEntity<ApiResponse<TokenResponse>> reissue(HttpServletRequest request) {
         String refreshToken = extractTokenFromHeader(request);
