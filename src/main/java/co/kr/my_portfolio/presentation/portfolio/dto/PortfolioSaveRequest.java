ee49b33571b94f0a0208eee88146e78f70b72f32
이건우 <qkskck3@gmail.com>
2025-06-08 16:19:28 +0900

    포트폴리오 저장 & 수정 Request Swagger Schema 추가

--- src/main/java/co/kr/my_portfolio/presentation/portfolio/dto/PortfolioSaveRequest.java
+++ src/main/java/co/kr/my_portfolio/presentation/portfolio/dto/PortfolioSaveRequest.java
@@ -1,5 +1,6 @@
 package co.kr.my_portfolio.presentation.portfolio.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
@@ -7,19 +8,24 @@ import lombok.NoArgsConstructor;
 
 import java.util.List;
 
+@Schema(description = "포트폴리오 등록 요청 DTO")
 @Getter
 @NoArgsConstructor
 public class PortfolioSaveRequest {
+    @Schema(description = "제목", example = "MP 프로젝트 포트폴리오")
     @NotBlank(message = "제목이 입력되지 않았습니다.")
     @Size(max = 100, message = "제목이 너무 깁니다.")
     private String title;
 
+    @Schema(description = "썸네일 URL", example = "https://example.com/image.jpg")
     @Size(max = 255, message = "썸네일 경로는 최대 255자까지 입력할 수 있습니다.")
     private String thumbnail;
 
+    @Schema(description = "상세 설명", example = "이 프로젝트는 포트폴리오를 등록/색인 하는 웹사이트 제작 프로젝트 입니다.")
     @NotBlank(message = "내용이 입력되지 않았습니다.")
     @Size(max = 10000, message = "내용은 최대 10000자까지 입력할 수 있습니다.")
     private String content;
 
+    @Schema(description = "태그 목록", example = "[\"AI\", \"SpringBoot\"]")
     private List<String> tags;
 }
--- src/main/java/co/kr/my_portfolio/presentation/portfolio/dto/PortfolioUpdateRequest.java
+++ src/main/java/co/kr/my_portfolio/presentation/portfolio/dto/PortfolioUpdateRequest.java
@@ -1,5 +1,6 @@
 package co.kr.my_portfolio.presentation.portfolio.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
@@ -11,19 +12,24 @@ import java.util.List;
 @Getter
 @NoArgsConstructor
 public class PortfolioUpdateRequest {
+    @Schema(description = "Portfolio_Id", example = "1")
     @NotNull(message = "portfolioId가 전달되지 않았습니다.")
     private Long portfolioId;
 
+    @Schema(description = "제목", example = "MP 프로젝트 포트폴리오")
     @NotBlank(message = "제목이 입력되지 않았습니다.")
     @Size(max = 100, message = "제목이 너무 깁니다.")
     private String title;
 
+    @Schema(description = "썸네일 URL", example = "https://example.com/image.jpg")
     @Size(max = 255, message = "썸네일 경로는 최대 255자까지 입력할 수 있습니다.")
     private String thumbnail;
 
+    @Schema(description = "상세 설명", example = "이 프로젝트는 포트폴리오를 등록/색인 하는 웹사이트 제작 프로젝트 입니다.")
     @NotBlank(message = "내용이 입력되지 않았습니다.")
     @Size(max = 10000, message = "내용은 최대 10000자까지 입력할 수 있습니다.")
     private String content;
 
+    @Schema(description = "태그 목록", example = "[\"AI\", \"SpringBoot\"]")
     private List<String> tags;
 }
