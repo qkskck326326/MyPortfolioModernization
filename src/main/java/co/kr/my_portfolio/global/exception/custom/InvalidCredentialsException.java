ae25552de49efa02ca5dd23568b46ab36ac39156
이건우 <qkskck3@gmail.com>
2025-07-31 16:09:08 +0900

    feat(auth): 인증 실패 상황 명확화를 위한 커스텀 예외 InvalidCredentialsException 추가

---
+++ src/main/java/co/kr/my_portfolio/global/exception/custom/InvalidCredentialsException.java
@@ -0,0 +1,12 @@
+package co.kr.my_portfolio.global.exception.custom;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.UNAUTHORIZED)
+public class InvalidCredentialsException extends RuntimeException {
+
+    public InvalidCredentialsException(String message) {
+        super(message);
+    }
+}
