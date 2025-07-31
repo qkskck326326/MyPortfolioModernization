53a7006d83316a63233413e83e93be0f270e60fc
이건우 <qkskck3@gmail.com>
2025-07-31 15:47:14 +0900

    feat(user): slug 기반 유저 조회, 중복 검사, 프로필 최소 정보 반환 기능 추가

--- src/main/java/co/kr/my_portfolio/application/user/UserService.java
+++ src/main/java/co/kr/my_portfolio/application/user/UserService.java
@@ -1,20 +1,26 @@
 package co.kr.my_portfolio.application.user;
 
+import co.kr.my_portfolio.domain.portfolio.queryRepository.PortfolioTagMappingQueryRepository;
 import co.kr.my_portfolio.infrastructure.security.AuthenticatedUser;
 import co.kr.my_portfolio.infrastructure.security.AuthenticatedUserProvider;
 import co.kr.my_portfolio.domain.auth.RefreshTokenRepository;
 import co.kr.my_portfolio.global.exception.custom.UserNotFoundException;
 import co.kr.my_portfolio.domain.user.Role;
 import co.kr.my_portfolio.domain.user.User;
-import co.kr.my_portfolio.presentation.user.dto.PasswordChangeRequest;
-import co.kr.my_portfolio.presentation.user.dto.UserProfileResponse;
-import co.kr.my_portfolio.presentation.user.dto.UserProfileUpdateRequest;
-import co.kr.my_portfolio.presentation.user.dto.UserSignupRequest;
+import co.kr.my_portfolio.infrastructure.user.SlugGenerator;
+import co.kr.my_portfolio.presentation.portfolio.dto.TagCountDto;
+import co.kr.my_portfolio.presentation.user.dto.*;
 import co.kr.my_portfolio.domain.user.UserRepository;
+import io.swagger.v3.oas.annotations.tags.Tags;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -24,6 +30,8 @@ public class UserService {
     private final PasswordEncoder passwordEncoder;
     private final RefreshTokenRepository refreshTokenRepository;
     private final AuthenticatedUserProvider authenticatedUserProvider;
+    private final SlugGenerator slugGenerator;
+    private final PortfolioTagMappingQueryRepository portfolioTagMappingQueryRepository;
 
     // 회원가입
     @Transactional
@@ -32,10 +40,15 @@ public class UserService {
             throw new IllegalArgumentException("이미 존재하는 이메일입니다.");
         }
 
+        if (userRepository.existsByNickname(request.getNickname())) {
+            throw new IllegalArgumentException("이미 존재하는 닉네임입니다.");
+        }
+
         User user = User.builder()
                 .email(request.getEmail())
                 .password(passwordEncoder.encode(request.getPassword()))
                 .nickname(request.getNickname())
+                .slug(slugGenerator.generate(request.getNickname()))
                 .role(Role.USER)
                 .userThumbnail(request.getUserThumbnail())
                 .introduce(request.getIntroduce())
@@ -48,7 +61,7 @@ public class UserService {
     
     // 유저 프로필 반환
     @Transactional
-    public UserProfileResponse getMyInfo() {
+    public UserProfileResponse  getMyInfo() {
         AuthenticatedUser authenticatedUser = authenticatedUserProvider.getAuthenticatedUser();
 
         Long userId = authenticatedUser.getId();
@@ -71,8 +84,8 @@ public class UserService {
         user.updateProfile(
                 request.getNickname(),
                 request.getUserThumbnail(),
+                request.getEmail(),
                 request.getIntroduce(),
-                request.getGithub(),
                 request.getBirth()
         );
 
@@ -98,5 +111,44 @@ public class UserService {
         refreshTokenRepository.deleteByUserId(userId);
     }
 
+    public boolean existsByNickname(String nickname) {
+        return userRepository.existsByNickname(nickname);
+    }
+
+    public boolean existsByEmail(String email) {
+        return userRepository.existsByEmail(email);
+    }
+
+    public UserInfoResponse getMyInfoForState() {
+        AuthenticatedUser authenticatedUser = authenticatedUserProvider.getAuthenticatedUser();
+        Long userId = authenticatedUser.getId();
+
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new UserNotFoundException("사용자를 찾을 수 없습니다"));
+
+        return UserInfoResponse.builder()
+                .nickname(user.getNickname())
+                .slug(user.getSlug())
+                .build();
+    }
+
+    @Transactional
+    public UserProfileAndTagsResponse getUserInfoBySlug(String slug) {
+        User user = userRepository.findBySlug(slug);
+        if (user == null) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "해당 유저를 찾을 수 없습니다.");
+        }
+        List<TagCountDto> tags = portfolioTagMappingQueryRepository.countTagsByUserId(user.getId());
+        return UserProfileAndTagsResponse.builder()
+                .userThumbnail(user.getUserThumbnail())
+                .birth(user.getBirth())
+                .github(user.getGithub())
+                .introduce(user.getIntroduce())
+                .slug(user.getSlug())
+                .email(user.getEmail())
+                .nickname(user.getNickname())
+                .tags(tags)
+                .build();
+    }
 }
 
